6f91c3b00d2ac93e68f6529d7e07fde73ca822e5
Ethan <hello@ethang.email>
2024-11-14 17:18:06 -0600

    fix lint

--- packages/eslint-config/build.ts
+++ packages/eslint-config/build.ts
@@ -1,5 +1,16 @@
+import { projectBuilder } from "../project-builder/src/project-builder.ts";
 import { updateReadme } from "./src/build/update-readme.js";
 import { updateRules } from "./src/build/update-rules.js";
 
 await updateRules();
 updateReadme();
+await projectBuilder(import.meta.dirname, {
+  entry: [
+    "src/eslint.config.js",
+    "src/config.astro.js",
+    "src/config.react.js",
+    "src/config.solid.js",
+    "src/constants.js",
+  ],
+  outDir: "dist",
+});
