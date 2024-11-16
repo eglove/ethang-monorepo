1100c9fadefe7a0dcccde1473214599ff0706527
Ethan <hello@ethang.email>
2024-11-16 12:19:16 -0600

    Add persistent caching to React Query client

--- apps/ethang-tanstack/src/clients/query.ts
+++ apps/ethang-tanstack/src/clients/query.ts
@@ -1,3 +1,25 @@
+import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
 import { QueryClient } from "@tanstack/react-query";
+import { del, get, set } from "idb-keyval";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 60 * 24,
+    },
+  },
+});
+
+export const persister = createAsyncStoragePersister({
+  storage: {
+    async getItem(key) {
+      return get(key);
+    },
+    async removeItem(key) {
+      return del(key);
+    },
+    async setItem(key, value) {
+      return set(key, value);
+    },
+  },
+});
--- apps/ethang-tanstack/src/components/providers.tsx
+++ apps/ethang-tanstack/src/components/providers.tsx
@@ -1,13 +1,13 @@
 import { NextUIProvider } from "@nextui-org/react";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { useNavigate } from "@tanstack/react-router";
 import constant from "lodash/constant.js";
 import { lazy, type PropsWithChildren } from "react";
 
-import { queryClient } from "../clients/query";
+import { persister, queryClient } from "../clients/query";
 
 const TanStackRouterDevtools =
-    "production" === process.env.NODE_ENV
+    "production" === process.env["NODE_ENV"]
       ? constant(null)
       : lazy(async () => {
         return import("@tanstack/router-devtools").then((result) => {
@@ -17,7 +17,7 @@ const TanStackRouterDevtools =
         });
       });
 
-const QueryDevtools = "production" === process.env.NODE_ENV
+const QueryDevtools = "production" === process.env["NODE_ENV"]
   ? constant(null)
   : lazy(async () => {
     return import("@tanstack/react-query-devtools").then((result) => {
@@ -31,7 +31,10 @@ export const Providers = ({ children }: Readonly<PropsWithChildren>) => {
   const navigate = useNavigate();
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
       <NextUIProvider navigate={(route) => {
         // eslint-disable-next-line @typescript-eslint/use-unknown-in-catch-callback-variable,no-console
         navigate({ to: route }).catch(console.error);
@@ -41,6 +44,6 @@ export const Providers = ({ children }: Readonly<PropsWithChildren>) => {
       </NextUIProvider>
       <QueryDevtools />
       <TanStackRouterDevtools position="bottom-left" />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   );
 };
