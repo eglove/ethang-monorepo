da636fcb41cc8ae176a5623c6e0486b223d559b5
Ethan <hello@ethang.email>
2024-11-16 20:05:50 -0600

    Remove MotionRoute component

--- apps/sterett-react/src/components/layouts/main-layout.tsx
+++ apps/sterett-react/src/components/layouts/main-layout.tsx
@@ -1,15 +1,12 @@
 import type { PropsWithChildren } from "react";
 
-import { MotionRoute } from "../motion-route.tsx";
 import { Navigation } from "../navigation/navigation.tsx";
 
 export const MainLayout = ({ children }: Readonly<PropsWithChildren>) => {
   return (
-    <MotionRoute>
-      <main>
-        <Navigation />
-        {children}
-      </main>
-    </MotionRoute>
+    <main>
+      <Navigation />
+      {children}
+    </main>
   );
 };
