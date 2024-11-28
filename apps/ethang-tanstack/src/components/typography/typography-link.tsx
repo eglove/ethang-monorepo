aaab8a6a6e874e37b3e44b75c97856e11b796139
Ethan <hello@ethang.email>
2024-11-27 20:04:44 -0600

    Add external link handling and update Node.js engine requirements
    Implemented a check to open external links in a new tab by default using `useMemo` and `lodash`'s `isString`. Updated `package.json` to require Node.js version 22 or higher.

--- apps/ethang-tanstack/src/components/typography/typography-link.tsx
+++ apps/ethang-tanstack/src/components/typography/typography-link.tsx
@@ -1,7 +1,11 @@
-import type { JSX, PropsWithChildren } from "react";
-
 import { cn } from "@/lib/utils.ts";
 import { Link, type LinkComponent } from "@tanstack/react-router";
+import isString from "lodash/isString.js";
+import {
+  type JSX,
+  type PropsWithChildren,
+  useMemo,
+} from "react";
 
 type TypographyLinkProperties = LinkComponent<"a"> & Omit<JSX.IntrinsicElements["a"], "target"> & PropsWithChildren<{
   className?: string;
@@ -12,9 +16,25 @@ export const TypographyLink = ({
   className,
   ...rest
 }: Readonly<TypographyLinkProperties>) => {
+  const isExternal = useMemo(() => {
+    if (isString(rest.href) && URL.canParse(rest.href) && "undefined" !== typeof globalThis) {
+      const url = new URL(rest.href);
+      const baseUrl = new URL(location.href);
+
+      if (baseUrl.host !== url.host) {
+        return true;
+      }
+    }
+
+    return false;
+  }, [rest.href]);
+
   return (
     <Link
       {...rest}
+      target={isExternal
+        ? "_blank"
+        : ""}
       className={cn("leading-7 underline underline-offset-2", className)}
       href={undefined}
       to={rest.href}
