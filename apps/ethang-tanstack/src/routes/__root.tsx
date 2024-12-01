a4be29000d032500fc29998f780175e11a30071c
Ethan <hello@ethang.email>
2024-12-01 16:09:50 -0600

    ts ignores

--- apps/ethang-tanstack/src/routes/__root.tsx
+++ apps/ethang-tanstack/src/routes/__root.tsx
@@ -1,8 +1,10 @@
 // eslint-disable-next-line react/naming-convention/filename
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 
+// @ts-expect-error it's css
 import "../index.css";
 
+// @ts-expect-error it's css
 import "@ethang/react-components/src/index.css";
 
 import { Providers } from "../components/providers";
