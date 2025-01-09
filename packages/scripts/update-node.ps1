42406ad9378257d382463a5ade0983467af157f4
Ethan <hello@ethang.email>
2025-01-08 20:39:01 -0600

    Update scripts to enhance global package management
    Added logic to update globally installed npm packages in `update.ps1` and removed redundant npm global installation commands from `update-node.ps1`. Streamlines maintenance and ensures up-to-date dependencies across tools.

--- packages/scripts/update-node.ps1
+++ packages/scripts/update-node.ps1
@@ -1,6 +1,5 @@
 $currentLocation = Get-Location
 Set-Location ~/projects/ethang-monorepo/packages/scripts
-npm i -g npm pnpm yarn typescript prisma nextui-cli bun
 Set-Location ~/projects/ethang-monorepo
 pnpm all
 Set-Location $currentLocation
--- packages/scripts/update.ps1
+++ packages/scripts/update.ps1
@@ -5,5 +5,13 @@ docker system prune -af
 
 # Visual Studio Update
 dotnet tool update -g dotnet-vs
+
+# NPM globals
+$packages = (npm outdated -g --depth=0 --parseable | ForEach-Object { ($_ -split '\\|/')[-1] }) | Select-Object -Unique
+foreach ($package in $packages) {
+    npm i -g $package
+}
+
+# Windows software update
 winget upgrade --unknown --all
 
