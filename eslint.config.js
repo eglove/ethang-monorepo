46ca500098a80d59fb1f7a42b20e2b51412be7e1
Ethan <hello@ethang.email>
2025-02-23 09:18:34 -0600

    version bump

--- eslint.config.js
+++ eslint.config.js
@@ -3,31 +3,31 @@ import tseslint from "typescript-eslint";
 import reactConfig from "@ethang/eslint-config/config.react.js";
 
 export default tseslint.config(
-    {
-        ignores: [
-            "**/*/dist",
-            "**/*/node_modules",
-            "packages/eslint-config/eslint.config.js",
-            "apps/**/*.gen.ts",
-            "**/*.d.ts",
-            "**/.sanity",
-            "**/storybook-static",
-            "**/convex/_generated",
-            "**/README.md"
-        ],
+  {
+    ignores: [
+      "**/*/dist",
+      "**/*/node_modules",
+      "packages/eslint-config/eslint.config.js",
+      "apps/**/*.gen.ts",
+      "**/*.d.ts",
+      "**/.sanity",
+      "**/storybook-static",
+      "**/convex/_generated",
+      "**/README.md",
+    ],
+  },
+  ...config,
+  ...reactConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-    ...config,
-    ...reactConfig,
-    {
-        languageOptions: {
-            parserOptions: {
-                project: true,
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
-    },
-    {
-        "files": ["packages/react-components/components/ui"],
-        "rules": {"react/prefer-read-only-props": "off"}
-    }
+  },
+  {
+    files: ["packages/react-components/components/ui"],
+    rules: { "react/prefer-read-only-props": "off" },
+  },
 );
