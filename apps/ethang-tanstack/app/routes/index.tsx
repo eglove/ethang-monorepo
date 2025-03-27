3572ed47107d8eb55b0555a7501f88c815f23335
Ethan <hello@ethang.email>
2025-03-26 19:40:52 -0500

    cleanup

--- apps/ethang-tanstack/app/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-const Home = () => {
-  return <button type="button">Add 1 to?</button>;
-};
-
-export const Route = createFileRoute("/")({
-  component: Home,
-});
