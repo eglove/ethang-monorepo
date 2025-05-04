8330e8b9b578118d606d70c2c0c56c9b8c2bc52b
Ethan <hello@ethang.email>
2025-05-04 11:03:25 -0500

    eslint

---
+++ templates/react-vite/src/app.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+      <h1>Vite + React + Cloudflare</h1>
+      <div>
+        <p>Hello</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
--- templates/react-vite/worker/index.ts
+++ templates/react-vite/worker/index.ts
@@ -1,12 +1,15 @@
+import startsWith from "lodash/startsWith.js";
+
 export default {
   fetch(request) {
     const url = new URL(request.url);
 
-    if (url.pathname.startsWith("/api/")) {
+    if (startsWith(url.pathname, "/api")) {
       return Response.json({
         name: "Cloudflare",
       });
     }
-		return new Response(null, { status: 404 });
+    return new Response(null, { status: 404 });
   },
+  // eslint-disable-next-line sonar/no-reference-error
 } satisfies ExportedHandler<Env>;
