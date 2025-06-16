1ae3baa220f82745a3ed20bceaf0fbf40f03d74b
ethang <hello@ethang.email>
2025-06-16 14:52:45 -0500

    add links and update text
    Signed-off-by: ethang <hello@ethang.email>

--- apps/ethang/src/components/tips/reactivity/reactivity-tables.tsx
+++ apps/ethang/src/components/tips/reactivity/reactivity-tables.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { Link } from "@heroui/react";
 
 import { TypographyH2 } from "../../typography/typography-h2.tsx";
 import { TypographyP } from "../../typography/typography-p.tsx";
@@ -6,14 +6,6 @@ import { OptimizedTable } from "./optimized-table.tsx";
 import { UnoptimizedTable } from "./unoptimized-table.tsx";
 
 export const ReactivityTables = () => {
-  useEffect(() => {
-    const script = globalThis.document.createElement("script");
-    script.src = "//unpkg.com/react-scan/dist/auto.global.js";
-    script.async = true;
-    // @ts-expect-error allow
-    document.body.append(script);
-  }, []);
-
   return (
     <>
       <TypographyH2>Unoptimized (Prop Drilling + Memoization)</TypographyH2>
@@ -27,12 +19,48 @@ export const ReactivityTables = () => {
         rerenders because on each data update, the table is getting a brand new
         array of table rows.
       </TypographyP>
+      <TypographyP>
+        This example uses HeroUI which caches results to{" "}
+        <Link
+          isExternal
+          showAnchorIcon
+          className="text-foreground"
+          href="https://www.heroui.com/docs/components/table#why-not-array-map"
+          underline="always"
+        >
+          "avoid re-rendering"
+        </Link>
+        . If you open React DevTools in your browser and turn on "Highlight
+        updates when components render," you'll see this doesn't actually work.
+      </TypographyP>
+      <TypographyP>
+        <Link
+          isExternal
+          showAnchorIcon
+          className="text-foreground"
+          href="https://github.com/eglove/ethang-monorepo/blob/master/apps/ethang/src/components/tips/reactivity/unoptimized-table.tsx"
+          underline="always"
+        >
+          Read the code
+        </Link>
+      </TypographyP>
       <UnoptimizedTable />
       <TypographyH2>Optimized (Cache Lookup)</TypographyH2>
       <TypographyP>
         In this example, instead of transforming all data at the top level, the
         individual components lookup or fetch their own data based on a "stable
         value" from the table. In this case the symbol.
+      </TypographyP>
+      <TypographyP>
+        <Link
+          isExternal
+          showAnchorIcon
+          className="text-foreground"
+          href="https://github.com/eglove/ethang-monorepo/blob/master/apps/ethang/src/components/tips/reactivity/optimized-table.tsx"
+          underline="always"
+        >
+          Read the code
+        </Link>
       </TypographyP>
       <OptimizedTable />
     </>
