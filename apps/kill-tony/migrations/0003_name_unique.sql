9ecbd352bd4935ba64f7ede9a055420eea94b12a
ethang <hello@ethang.email>
2025-07-23 16:45:46 -0500

    chore: remove kill-tony app and all related files
    - Deleted ppearance/.tsx, __root.tsx, dd-appearance-form.tsx, and associated components
    - Removed Apollo client setup and GraphQL queries/mutations
    - Cleared migrations, prisma models, and generated files in kill-tony app
    - Updated .gitignore to clean up ignored directories and files

--- apps/kill-tony/migrations/0003_name_unique.sql
+++
@@ -1,3 +0,0 @@
--- Migration number: 0003 	 2025-07-16T03:57:19.948Z
--- CreateIndex
-CREATE UNIQUE INDEX "Appearance_name_key" ON "Appearance"("name");
--- apps/kill-tony/worker/prisma-client.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaD1 } from "@prisma/adapter-d1";
-
-import { PrismaClient } from "../generated/prisma/client";
-
-export const getPrismaClient = (environment: Env) => {
-  const adapter = new PrismaD1(environment.DB);
-  return new PrismaClient({ adapter });
-};
