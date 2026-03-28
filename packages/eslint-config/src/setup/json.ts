156714e122ae80882bce7afbd74b39ceee8cadd8
ethang <hello@ethang.email>
2026-03-28 11:37:55 -0500

    feat(eslint-config): add Plugin instances to file-type and tailwind setup files

--- packages/eslint-config/src/setup/json.ts
+++ packages/eslint-config/src/setup/json.ts
@@ -1,6 +1,7 @@
 import json from "@eslint/json";
 import keys from "lodash/keys.js";
 
+import { Plugin } from "../build/plugin.ts";
 import {
   type CustomRules,
   type EsLintRules,
@@ -15,3 +16,43 @@ const ruleNames = keys(
 const customRules: CustomRules = [];
 
 export const jsonRules = genRules(ruleNames, customRules, "json");
+
+const eslintJson = "@eslint/json";
+const eslintJsonUrl = "https://github.com/eslint/json";
+const jsonImportString = 'import json from "@eslint/json";';
+
+export const jsonPlugin = new Plugin({
+  files: "**/*.json",
+  importString: jsonImportString,
+  language: "json/json",
+  name: eslintJson,
+  order: 0,
+  pluginName: "json",
+  pluginValue: "json",
+  rules: jsonRules,
+  url: eslintJsonUrl,
+});
+
+export const jsoncPlugin = new Plugin({
+  files: "**/*.jsonc",
+  importString: jsonImportString,
+  language: "json/jsonc",
+  name: eslintJson,
+  order: 0,
+  pluginName: "json",
+  pluginValue: "json",
+  rules: jsonRules,
+  url: eslintJsonUrl,
+});
+
+export const json5Plugin = new Plugin({
+  files: "**/*.json5",
+  importString: jsonImportString,
+  language: "json/json5",
+  name: eslintJson,
+  order: 0,
+  pluginName: "json",
+  pluginValue: "json",
+  rules: jsonRules,
+  url: eslintJsonUrl,
+});
--- packages/eslint-config/src/setup/tailwind.ts
+++ packages/eslint-config/src/setup/tailwind.ts
@@ -1,6 +1,7 @@
 import tailwind from "eslint-plugin-tailwindcss";
 import keys from "lodash/keys.js";
 
+import { Plugin } from "../build/plugin.ts";
 import { genRules, getNonDeprecatedRules } from "./gen-rules.ts";
 
 const ruleNames = keys(getNonDeprecatedRules(tailwind.rules));
@@ -10,3 +11,16 @@ const customRules = [
 ];
 
 export const tailwindRules = genRules(ruleNames, customRules, "tailwind");
+
+export const tailwindPlugin = new Plugin({
+  extraOptions: "settings: { tailwindcss: { config: pathToConfig } },",
+  files: "**/*.{js,ts,jsx,tsx,cjs,cts,mjs,mts}",
+  importString:
+    'import { fixupPluginRules } from "@eslint/compat";\nimport tailwind from "eslint-plugin-tailwindcss";',
+  name: "eslint-plugin-tailwindcss",
+  order: 0,
+  pluginName: "tailwind",
+  pluginValue: "fixupPluginRules(tailwind)",
+  rules: tailwindRules,
+  url: "https://github.com/francoismassart/eslint-plugin-tailwindcss",
+});
