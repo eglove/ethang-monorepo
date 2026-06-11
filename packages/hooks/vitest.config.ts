4eb64bcce7ab4df1e06a7befa9648d42f8870f67
Ethan Glover <hello@ethang.email>
2026-06-10 21:37:22 -0500

    refactor: update Vitest configuration for hooks and leetcode
    - adjust coverage thresholds in `packages/hooks/vitest.config.ts`
    - change coverage include from specific hooks to entire `src` directory
    - fine‑tune coverage percentages for branches, functions, lines, and statements
    - increment line/statement thresholds in `packages/leetcode/vitest.config.ts` for more
    
    Signed-off-by: Ethan Glover <hello@ethang.email>

--- packages/hooks/vitest.config.ts
+++ packages/hooks/vitest.config.ts
@@ -3,15 +3,15 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      include: ["src/use-animation-interval.ts", "src/use-map.ts", "src/use-boolean.ts"],
+      include: ["src"],
       provider: "v8",
       reporter: ["text", "json", "html", "lcov"],
       thresholds: {
         autoUpdate: true,
-        branches: 100,
-        functions: 100,
-        lines: 100,
-        statements: 100,
+        branches: 11.39,
+        functions: 15.09,
+        lines: 16.03,
+        statements: 16.03,
       },
     },
     environment: "jsdom",
