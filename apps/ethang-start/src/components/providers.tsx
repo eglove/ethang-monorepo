98ed7cea11e9700d354380874223a129d2afb331
Ethan Glover <hello@ethang.email>
2026-08-06 17:32:59 -0500

    sanity text
    Signed-off-by: Ethan Glover <hello@ethang.email>

--- apps/ethang-start/src/components/providers.tsx
+++ apps/ethang-start/src/components/providers.tsx
@@ -1,33 +1,17 @@
 import type { PropsWithChildren } from "react";
 
-import {
-  type Link as AstryxLink,
-  LinkProvider,
-  Theme
-} from "@astryxdesign/core";
+import { LinkProvider, Theme } from "@astryxdesign/core";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Link } from "@tanstack/react-router";
 
 import { getContext } from "../router.tsx";
 import { nightowlTheme } from "../themes/nightowl.js";
+import { HybridLink } from "./hybrid-link.tsx";
 
 export const Providers = ({ children }: Readonly<PropsWithChildren>) => {
   return (
     <Theme mode="dark" theme={nightowlTheme}>
       <QueryClientProvider client={getContext().queryClient}>
-        <LinkProvider
-          component={(properties: Parameters<typeof AstryxLink>[0]) => {
-            const url = properties.href ?? "#";
-
-            return (
-              <Link to={url} target={properties.target ?? "_self"}>
-                {properties.children}
-              </Link>
-            );
-          }}
-        >
-          {children}
-        </LinkProvider>
+        <LinkProvider component={HybridLink}>{children}</LinkProvider>
       </QueryClientProvider>
     </Theme>
   );
